9143a8f3c7bbdeaed53e0cc4bdf9d73115517981
Fraser W Levack <2777042L@student.gla.ac.uk>
2024-03-01 16:13:30 +0000

    Add - New custom template tag to show tunes by user. Then added to profile.html. Fixed - Trying to show profile picture when none is uploaded.

--- django_jam_app/templatetags/django_jam_template_tags.py
+++ django_jam_app/templatetags/django_jam_template_tags.py
@@ -4,16 +4,21 @@ from django_jam_app.models import Tune
 register = template.Library()
 
 
-@register.inclusion_tag('django_jam_app/tunes.html')
-def get_tune_list(current_tune=None):
+@register.inclusion_tag('django_jam_app/tunes.html')    # Get all tunes
+def get_tune_list():
     return {'Tunes': Tune.objects.all()}
 
 
-@register.filter(name='is_current_user')
+@register.filter(name='is_current_user')            # Check if user is current user (logged in)
 def is_current_user(user_profile, current_user):
     return user_profile.user == current_user
 
 
-@register.filter(name='count_tunes')
+@register.filter(name='count_tunes')                # Count tunes by user
 def count_tunes(user):
     return Tune.objects.filter(artist=user).count()
+
+
+@register.inclusion_tag('django_jam_app/tunes.html')    # Get tunes by user
+def get_tunes_by_user(user):
+    return {'Tunes': Tune.objects.filter(artist=user)}
