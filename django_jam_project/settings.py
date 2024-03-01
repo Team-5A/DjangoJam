d70b971c79c17b482850c329746d9ec29e5c1cf7
Su1phur <icooltse@gmail.com>
2024-03-01 09:42:03 +0000

    Gitignore and added forward slash to Static_URL

--- django_jam_project/settings.py
+++ django_jam_project/settings.py
@@ -119,11 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATICFILES_DIRS = [STATIC_DIR, ]
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 #media files
 MEDIA_ROOT = MEDIA_DIR
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
