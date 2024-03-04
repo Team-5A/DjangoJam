5dbfa567a3216469cfd65a070ef9909c6c780057
Freddie Nelson <47699795+freddie-nelson@users.noreply.github.com>
2024-03-04 18:47:50 +0000

    chore: migration script

---
+++ migrate.py
@@ -0,0 +1,10 @@
+import os
+
+if os.path.exists("db.sqlite3"):
+    os.remove("db.sqlite3")
+
+os.system("python manage.py makemigrations django_jam_app")
+os.system("python manage.py migrate")
+os.system("python manage.py createsuperuser --username admin --email admin@admin.co.uk")
+os.system("python populate_jam.py")
+
