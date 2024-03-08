0fb3edebbb6c8053434df1c8b6c898267e2e680c
Freddie Nelson <47699795+freddie-nelson@users.noreply.github.com>
2024-03-08 15:44:17 +0000

    feat: register page and functionality as well as improved form errors

--- static/JS/auth.js
+++ static/JS/auth.js
@@ -1,14 +1,27 @@
-const showPasswordButton = document.getElementById("show-password-button");
+const showPasswordButtons = Array.from(document.querySelectorAll(".show-password-button"));
 
-showPasswordButton.addEventListener("click", () => {
-  const inputName = showPasswordButton.getAttribute("for");
-  const input = document.querySelector(`input[name="${inputName}"]`);
+showPasswordButtons.forEach((showPasswordButton) => {
+  showPasswordButton.addEventListener("click", () => {
+    const inputName = showPasswordButton.getAttribute("for");
+    const input = document.querySelector(`input[name="${inputName}"]`);
 
-  if (input.type === "password") {
-    input.type = "text";
-    showPasswordButton.textContent = "hide password";
-  } else {
-    input.type = "password";
-    showPasswordButton.textContent = "show password";
-  }
+    if (input.type === "password") {
+      input.type = "text";
+      showPasswordButton.textContent = "hide password";
+    } else {
+      input.type = "password";
+      showPasswordButton.textContent = "show password";
+    }
+  });
 });
+
+// prefill username if provided in query string
+const usernameInput = document.querySelector("input[name='username']");
+if (usernameInput) {
+  const urlParams = new URLSearchParams(window.location.search);
+
+  const username = urlParams.get("username");
+  if (username) {
+    usernameInput.value = username;
+  }
+}
