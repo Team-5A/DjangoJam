adacf3aba908087082a56ffbeb45bf1c123413df
Freddie Nelson <47699795+freddie-nelson@users.noreply.github.com>
2024-03-09 17:09:04 +0000

    feat: like button styles

--- static/JS/tune.js
+++ static/JS/tune.js
@@ -14,4 +14,30 @@ tunes.forEach((tune) => {
       alert(`Failed to delete tune: ${error}`);
     }
   });
+
+  const likeButton = document.querySelector(".tune-like");
+  const likeImage = document.querySelector(".like-image");
+  const unlikeImage = document.querySelector(".unlike-image");
+
+  const updateLikeButton = () => {
+    if (likeButton.classList.contains("liked")) {
+      likeImage.style.display = "none";
+      unlikeImage.style.display = "block";
+    } else {
+      likeImage.style.display = "block";
+      unlikeImage.style.display = "none";
+    }
+  };
+  updateLikeButton();
+
+  likeButton.addEventListener("click", async () => {
+    try {
+      //   await fetch(`/django_jam_app/like_tune/${tuneId}`);
+
+      likeButton.classList.toggle("liked");
+      updateLikeButton();
+    } catch (error) {
+      alert(`Failed to like tune: ${error}`);
+    }
+  });
 });
