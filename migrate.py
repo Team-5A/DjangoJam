e8faa0e2d0fd0e1e812c7b67dd278d6a67e2a455
Fraser W Levack <2777042L@student.gla.ac.uk>
2024-03-10 22:48:19 +0000

    Fix : Added create_super_user.py script that gives admin a profile when created.

--- migrate.py
+++ migrate.py
@@ -5,7 +5,6 @@ if os.path.exists("db.sqlite3"):
 
 os.system("python manage.py makemigrations django_jam_app")
 os.system("python manage.py migrate")
-os.system("python manage.py createsuperuser --username admin --email admin@admin.co.uk")
+os.system("python create_super_user.py")
 
 os.system("python populate_jam.py")
-
