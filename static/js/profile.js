22bdb263ccf3f5227d8bd1f3b8d9ff6037d45ce0
Freddie Nelson <47699795+freddie-nelson@users.noreply.github.com>
2024-03-12 14:45:00 +0000

    feat: compress profile images

--- static/js/profile.js
+++ static/js/profile.js
@@ -13,9 +13,46 @@ const profilePictureImg = document.querySelector(".profile-picture img");
 const profilePictureInput = document.getElementById("upload-picture");
 const csrfToken = document.querySelector("input[name=csrfmiddlewaretoken]").value;
 
+const compressImage = async (file) => {
+  // const fileReader = new FileReader();
+  // fileReader.readAsDataURL(file);
+
+  // await new Promise((resolve) => {
+  //   fileReader.onload = (e) => {
+  //     resolve();
+  //   };
+  // });
+
+  // const buffer = await file.arrayBuffer();
+  // const blob = new Blob(new Uint8Array(buffer));
+  const image = await createImageBitmap(file);
+
+  const offscreenCanvas = new OffscreenCanvas(500, 500);
+  const ctx = offscreenCanvas.getContext("2d");
+
+  ctx.drawImage(image, 0, 0, 500, 500);
+
+  const canvasBlob = await offscreenCanvas.convertToBlob({
+    quality: 0.5,
+    type: "image/jpeg",
+  });
+
+  return canvasBlob;
+};
+
 profilePictureInput.addEventListener("input", async (e) => {
+  if (
+    profilePictureInput.files[0].type !== "image/jpeg" &&
+    profilePictureInput.files[0].type !== "image/png"
+  ) {
+    alert("Error: Profile picture must be a png or jpeg.");
+    return;
+  }
+
+  const compressed = await compressImage(profilePictureInput.files[0]);
+
   const data = new FormData();
-  data.append("profile_picture", profilePictureInput.files[0]);
+  data.append("profile_picture", compressed, profilePictureInput.files[0].name);
 
   try {
     const res = await fetch("/django_jam_app/upload_profile_picture/", {
@@ -29,6 +66,7 @@ profilePictureInput.addEventListener("input", async (e) => {
     const json = await res.json();
     if (json.error) throw new Error(json.error);
 
+    console.log(json.picture_url);
     profilePictureImg.src = json.picture_url;
   } catch (error) {
     alert(error);
